0e02bcd74a2cb6eac4783bf8c2063a6060967d81
anwerr357 <103385636+anwerr357@users.noreply.github.com>
2023-02-22 18:18:15 +0100

    Update moncalcul.js

--- moncalcul.js
+++ moncalcul.js
@@ -5,52 +5,7 @@ var somme=document.querySelector('.long >button');
 var soustracter=document.querySelector('.regular >#regular-button');
 var egale=document.querySelector('.regular >button');
 
-// outpout.textContent="0";
-// boutons[0].onclick= function()
-// {if(outpout.textContent=='0')
-// outpout.textContent='0';
-// if(outpout.textContent.length<18)
-// {
-// outpout.textContent=outpout.textContent+'7';
 
-// }}
-// boutons[1].onclick= function()
-// {
-// if(outpout.textContent=='0')
-// outpout.textContent='0';
-
-// if(outpout.textContent.length<18)
-// {
-// outpout.textContent=outpout.textContent+'8';
-
-// }
-// }
-
-// boutons[2].onclick= function()
-// {
-// if(outpout.textContent=='0')
-// outpout.textContent='0';
-// if(outpout.textContent.length<18)
-// {
-// outpout.textContent=outpout.textContent+'9';
-
-// }}
-// clear[0].onclick=function()
-// {
-//        if (outpout.textContent=='0')
-//     outpout.textContent='0';
- 
-//     else
-//     {
-//        outpout.textContent =  outpout.textContent.substring(1);
-    
-//    }
-// }
-//mystring.replaceAt( class="hljs-number">4, string">'')
-
-//string.substr(start, length)
-/*ch =' 6666 + 5555 '
- ch.split('+')*/
 output.textContent = '0';
 
 for(let i=0; i< boutons.length ; i++){
@@ -128,39 +83,3 @@ function enabled(){
 
 
 
-
-
-/*egale.addEventListener('click',()=>{
-    //sentence.includes(word) ? 'is'
-   
-    
-    const tab=output.textContent.split('+','-');
-    var res=0;
-    for(let i=0;i<tab.length;i++)
- {
-  res =res + Number(tab[i]);
- }
-output.textContent=res;
-})
-
-
-
-//Number( output.textContent) + Number( boutons[i].innerText)
-/*var chifrre=0;
-for(let i=0; i< boutons.length ; i++){
-    boutons[i].addEventListener('click', () => {
-    chiffre=parseInt(boutons[i]);
-    
-    })}  
-nb=0;
-somme.addEventListener("click",()=>{
-     nb=parseInt(textContent);
-    nb=nb+chiffre;
-    textContent=nb;
-})
-
-
-//boutons.addEventListener('click', () =>{
-
-
-*/
